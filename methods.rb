4d17256c35dc9c79f88ab3943370455dc6b50bb0
Calebbii <biicaleb316@gmail.com>
2022-09-22 21:36:02 +0300

    lab completed

--- methods.rb
+++ methods.rb
@@ -1,1 +1,29 @@
 # Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end    
+greet_programmer
+
+
+def greet (name)
+    puts "Hello, #{name}!"
+end
+greet("")
+
+
+def greet_with_default (name= "Hello, programmer!")
+    puts "Hello, #{name}!"
+end
+greet_with_default()
+
+def add (num1, num2)
+    return num1 +num2
+end
+add(4,4)
+
+def halve(int)
+    if int.class != Integer
+      return nil
+    end
+    int/2
+end
